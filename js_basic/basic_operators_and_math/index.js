c0837e1353e607e82af6e900490634cfbace6e92
blockSuho <akakak695@naver.com>
2025-11-09 20:44:28 +0900

    feat:Operator Finish

---
+++ js_basic/basic_operators_and_math/index.js
@@ -0,0 +1,65 @@
+// 연산자
+// 여기서 연산자는 + 연산자
+// 피연산자는 a,b
+let a = 10;
+let b = 2;
+let c = a + b;
+console.log(c);
+
+
+// 덧셈 연산자
+let kim = 100
+let lee = 200
+let result = a + b;
+console.log("더한값",result);
+
+// 뺄셈 연산자 
+let num1 = 100
+let num2 = 200
+let total = num1 - num2;
+console.log("마이너스 값",total);
+
+// 곱셈 연산자
+let num3 = 30;
+let num4 = 6;
+let num5 = num3 * num4;
+console.log("곱한 값",num5);
+
+// 나눗셈 연산자
+let num6 = 20;
+let num7 = 4;
+let half =  num6 / num7;
+console.log("나눈 값",half)
+
+// 나머지 연산자
+let num9 = 200;
+let num10 = 40;
+let remainder = num9 % num10;
+console.log("나머지 값",remainder);
+
+// 거듭제곱 연산자
+let n1 = 10;
+let n2 = 20;                                 
+let square = n1 ** n2;
+console.log("거듭제곱 값",square);
+
+
+// 증가 연산자
+let count = 1250;
+count++
+console.log(count);
+
+// 감소 연산자
+let count2 = 2000;
+count2--
+console.log(count2);
+
+// 전위 연산자
+let a2 = 2000;
+++a2;
+console.log(a2) // 2001
+
+// 후위 연산자
+let b2 = 3022;
+let d = b2++;
+console.log(b2) // 결과는 b = 3022 , d = 3022
