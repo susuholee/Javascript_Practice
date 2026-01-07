d8ae2010e01c775f86b22f25e4b9a60ce907f74e
blockSuho <akakak695@naver.com>
2026-01-07 22:11:04 +0900

    feat: 메서드 && this 학습 추가

---
+++ Object/Method&&this/index.js
@@ -0,0 +1,11 @@
+// let person = {
+//     name : "수호",
+//     age : 26,
+//     city : "남양주",
+//     sayTalk(){
+//         alert(`안녕하세요 저는 ${this.name}입니다. 나이는 ${this.age}세 입니다. 거주하는 지역은 ${this.city}입니다.`)
+//     }
+// }
+
+// person.sayTalk();
+
